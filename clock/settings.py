9f45d96fde1c78065c5ef7c5d484146ad155b229
Gaurav Kumar <gauravbaba311@gmail.com>
2026-03-05 16:24:14 +0530

    Update settings for production configuration
    Changed DEBUG setting to False and updated ALLOWED_HOSTS to allow all hosts. Added STATIC_ROOT for static files.

--- clock/settings.py
+++ clock/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dn-j9ujyp^t8(&1d6d_1+ofge4gip52frx7285&=dpb9b#3-%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
